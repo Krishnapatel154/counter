cf26e4068e3d8d9a4b98f709455e1a50614b035d
Krishnapatel154 <157212290+Krishnapatel154@users.noreply.github.com>
2024-03-20 01:59:03 +0000

    data

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from 'react'
 function App() {
-   
+  const [data,Setdata]=useState(0)
+    function counter()
+    {
+      Setdata(data+1)
+    }
+    function counter1()
+    {
+      Setdata(data-1)
+    }
   return (
     <div className="App">
       <header className="App-header">
-     <h1>counter:</h1>
-     <button>push me  </button>
+     <h1>counter:{data}</h1>
+     <button onClick={counter}>inscre</button>
+     <br></br>
+     <button onClick={counter1}>descre</button>
       </header>
     </div>
   );
 }
-
 export default App;
